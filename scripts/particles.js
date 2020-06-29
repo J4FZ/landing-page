84c04a380f1876af091d3b549cdff0afc15d1015
James Frazer <31794629+J4FZ@users.noreply.github.com>
2020-06-29 14:44:32 +0100

    Update particles.js

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('animation'),
+var canvas = document.getElementById('particles'),
    can_w = parseInt(canvas.getAttribute('width')),
    can_h = parseInt(canvas.getAttribute('height')),
    ctx = canvas.getContext('2d');
