e07d830f116a1ef7e600339f7945942919467a11
James Frazer <31794629+J4FZ@users.noreply.github.com>
2020-07-19 13:56:23 +0100

    Update particles.js

--- scripts/particles.js
+++ scripts/particles.js
@@ -273,17 +273,7 @@ $(document).ready(function() {
             }
             
         }); 
-       $('.hidemeport').each( function(i){
-                             var bottom_of_object = ($(this).position().top + $(this).outerHeight()) * 0.6;
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the top 20% of the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object){
-                
-                $(this).animate({'opacity':'1'},1500);
-                    
-            }
-                             });
+      
        
       });
 });
