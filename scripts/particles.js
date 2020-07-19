d709f8f3efa3529bee4b42aaf649afbbcc81c5f6
James Frazer <31794629+J4FZ@users.noreply.github.com>
2020-07-19 13:59:05 +0100

    Update particles.js

--- scripts/particles.js
+++ scripts/particles.js
@@ -263,7 +263,7 @@ $(document).ready(function() {
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
             
-            var bottom_of_object = $(this).position().top;// + $(this).outerHeight();
+            var bottom_of_object = $(this).position().top * 1.1;// + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_object ){
